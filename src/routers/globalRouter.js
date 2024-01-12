533ce6d7d5cead7c833164d609354c16298c984b
Yang_U_Seok <USYang123@ghithub.com>
2024-01-12 12:35:51 +0900

    chapter 6 중간

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -5,7 +5,7 @@ import { trending } from "../controllers/videoController"
 const globalRouter = express.Router();
 
 globalRouter.get("/join", join);
-globalRouter.get('/home', trending);
+globalRouter.get('/', trending);
 globalRouter.get("/login", login);
 export default globalRouter;
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,7 +3,8 @@ import {
     watch,
     getEdit, 
     postEdit,
-    upload, 
+    getUpload, 
+    postUpload,
     deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,7 +13,7 @@ const videoRouter = express.Router();
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", getEdit);
 videoRouter.post("/:id(\\d+)/edit", postEdit);
-
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 //videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); // 이렇게 써도 같음
 
 
